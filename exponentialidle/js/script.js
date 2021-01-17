d8dd8db1e5a3cf5dda8f2c834c2da2d415cb6b2b
Gilles-Philippe Paillé <gilles-philippe.paille@vyv.ca>
2021-01-17 13:18:02 -0500

    fixes

--- exponentialidle/js/script.js
+++ exponentialidle/js/script.js
@@ -1,19 +1,3 @@
-// Automatic Slideshow - change image every 4 seconds
-var carouselIndex = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("screenshots");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  carouselIndex++;
-  if (carouselIndex > x.length) {carouselIndex = 1}    
-  x[carouselIndex-1].style.display = "block";  
-  setTimeout(carousel, 4000);    
-}
-
 // Used to toggle the menu on small screens when clicking on the menu button
 function toggleMenu() {
   var x = document.getElementById("navDemo");
@@ -30,22 +14,4 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
-
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("screenshots");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
 }
